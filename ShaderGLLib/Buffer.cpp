fc1f820df1b73e1fea5533a023148addd9a6f50c
JZJeff <jeff.zufferey@students.hevs.ch>
2020-03-24 11:47:01 +0100

    Buffer test passed

--- ShaderGLLib/Buffer.cpp
+++ ShaderGLLib/Buffer.cpp
@@ -6,27 +6,33 @@ namespace sgl {
 
 
 	Buffer::Buffer(
-		const BufferType buffer_type /*= BufferType::ARRAY_BUFFER*/, 
+		const BufferType buffer_type /*= BufferType::ARRAY_BUFFER*/,
 		const BufferUsage buffer_usage /*= BufferUsage::STATIC_DRAW*/) :
 		buffer_type_(buffer_type),
 		buffer_usage_(buffer_usage)
+
 	{
 #pragma message ("You have to complete this code!")
+
+		glGenBuffers(1, &buffer_object_);
 	}
 	
 	Buffer::~Buffer()
 	{
 #pragma message ("You have to complete this code!")	
+		glDeleteBuffers(1, &buffer_object_);
 	}
 
 	void Buffer::Bind() const
 	{
-#pragma message ("You have to complete this code!")	
+#pragma message ("You have to complete this code!")
+		glBindBuffer(static_cast<GLenum>(buffer_type_), buffer_object_ );
 	}
 
 	void Buffer::UnBind() const
 	{
 #pragma message ("You have to complete this code!")	
+		glBindBuffer(static_cast<GLenum>(buffer_type_), 0);
 	}
 
 	void Buffer::BindCopy(
@@ -34,6 +40,16 @@ namespace sgl {
 		const void* data /*= nullptr*/) const
 	{
 #pragma message ("You have to complete this code!")	
+
+		Bind();
+
+		glBufferData(
+			static_cast<GLenum>(buffer_type_),
+			size,
+			&data,
+			GL_STATIC_DRAW);
+
+		UnBind();
 	}
 
 } // End namespace sgl.
