693e1917f55030287e9506e5fe4adf279ac1c900
JZJeff <jeff.zufferey@students.hevs.ch>
2020-03-24 14:23:49 +0100

    ProgramTest passed

--- ShaderGLLib/Buffer.cpp
+++ ShaderGLLib/Buffer.cpp
@@ -10,28 +10,24 @@ namespace sgl {
 		const BufferUsage buffer_usage /*= BufferUsage::STATIC_DRAW*/) :
 		buffer_type_(buffer_type),
 		buffer_usage_(buffer_usage)
-
 	{
-#pragma message ("You have to complete this code!")
-
 		glGenBuffers(1, &buffer_object_);
 	}
 	
 	Buffer::~Buffer()
 	{
-#pragma message ("You have to complete this code!")	
 		glDeleteBuffers(1, &buffer_object_);
 	}
 
 	void Buffer::Bind() const
 	{
-#pragma message ("You have to complete this code!")
+
 		glBindBuffer(static_cast<GLenum>(buffer_type_), buffer_object_ );
 	}
 
 	void Buffer::UnBind() const
 	{
-#pragma message ("You have to complete this code!")	
+
 		glBindBuffer(static_cast<GLenum>(buffer_type_), 0);
 	}
 
@@ -39,8 +35,6 @@ namespace sgl {
 		const size_t size, 
 		const void* data /*= nullptr*/) const
 	{
-#pragma message ("You have to complete this code!")	
-
 		Bind();
 
 		glBufferData(
--- ShaderGLLib/Program.cpp
+++ ShaderGLLib/Program.cpp
@@ -1,67 +1,92 @@
 #include "Program.h"
+#include <map>
+#include <iostream>
 #include <stdexcept>
 
 namespace sgl {
 
 	Program::Program()
 	{
-#pragma message ("You have to complete this code!")
+		program_id_ = glCreateProgram();
+
+		if (program_id_ <= 0)
+		{
+			throw std::runtime_error("Could not have a program that is <= 0");
+		}
 	}
 
 	Program::~Program()
 	{
-#pragma message ("You have to complete this code!")
+		glDeleteProgram(program_id_);
 	}
 
 	void Program::AddShader(const Shader& shader)
 	{
-#pragma message ("You have to complete this code!")
+
+		glAttachShader(program_id_, shader.GetId());
 	}
 
 	void Program::LinkShader()
 	{
-#pragma message ("You have to complete this code!")
+		glLinkProgram(program_id_);
 	}
 
 	void Program::Use() const
 	{
-#pragma message ("You have to complete this code!")
+		glUseProgram(program_id_);
 	}
 
 	void Program::UniformBool(const std::string& name, bool value) const
 	{
-#pragma message ("You have to complete this code!")
+		glUniform1i(
+			glGetUniformLocation(program_id_, name.c_str()), (int)value);
+
 	}
 
 	void Program::UniformInt(const std::string& name, int value) const
 	{
-#pragma message ("You have to complete this code!")
+		glUniform1i(
+			glGetUniformLocation(program_id_, name.c_str()),
+			value);
 	}
 
 	void Program::UniformFloat(const std::string& name, float value) const
 	{
-#pragma message ("You have to complete this code!")
+		glUniform1f(glGetUniformLocation(program_id_, name.c_str()), value);
 	}
 
 	void Program::UniformVector2(
 		const std::string& name, 
 		const sgl::vector2& vec2) const
 	{
-#pragma message ("You have to complete this code!")
+		glUniform2f(
+			glGetUniformLocation(program_id_, name.c_str()), vec2.x, vec2.y);
+
 	}
 
 	void Program::UniformVector3(
 		const std::string& name, 
 		const sgl::vector3& vec3) const
 	{
-#pragma message ("You have to complete this code!")
+		glUniform3f(
+			glGetUniformLocation(program_id_, name.c_str()),
+			vec3.x,
+			vec3.y,
+			vec3.z);
+
 	}
 
 	void Program::UniformVector4(
 		const std::string& name, 
 		const sgl::vector4& vec4) const
 	{
-#pragma message ("You have to complete this code!")
+		glUniform4f(
+			glGetUniformLocation(program_id_, name.c_str()),
+			vec4.x,
+			vec4.y,
+			vec4.z,
+			vec4.w);
+
 	}
 
 	void Program::UniformMatrix(
@@ -69,12 +94,50 @@ namespace sgl {
 		const sgl::matrix& mat,
 		const bool transpose /*= false*/) const
 	{
-#pragma message ("You have to complete this code!")
+
+		glUniformMatrix4fv(
+			glGetUniformLocation(program_id_, name.c_str()),
+			1,
+			transpose ? GL_TRUE : GL_FALSE,
+			&mat._11);//verifier si ca plante plus loin
+
 	}
 
 	const int Program::GetMemoizeUniformLocation(const std::string& name) const
 	{
-#pragma message ("You have to complete this code!")
+
+		
+		// Create a map iterator and point to beginning of map
+		std::map<std::string, int>::iterator index = memoize_map_.begin();
+
+		auto search = memoize_map_.find(name);
+
+		//// Iterate over the map using Iterator till end.
+		//while (index != memoize_map_.end())
+		//{
+		//	//Check  value existe return it 
+		//	if (search != memoize_map_.end()) {
+		//		return index->second;
+		//	}
+		//	//add it
+		//	else {
+		//		auto tmp = std::pair<std::string, int>(name.c_str(), program_id_);
+		//		memoize_map_.insert(tmp);
+		//	}
+		//	// Increment the Iterator to point to next entry
+		//	index++;
+		//}
+
+		//Check  value existe return it 
+		if (search != memoize_map_.end()) {
+			return index->second;
+		}
+		//add it
+		else {
+			auto tmp = std::pair<std::string, int>(name.c_str(), program_id_);
+			memoize_map_.insert(tmp);
+		}
+			
 		return -1;
 	}
 
